c9e7cfbd63073ab7c61f10477e45baf0d8b39102
unknown <lochaydeguerreiro@hotmail.com>
2025-09-06 02:14:39 -0300

    Correção HTaccess 2

--- backup-20250905-232059/libido-renovada-backup/status.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=UTF-8');
-echo "OK\n";
-echo 'Time: ' . date('c') . "\n";
-echo 'IP: ' . ($_SERVER['REMOTE_ADDR'] ?? '') . "\n";
-echo 'UA: ' . ($_SERVER['HTTP_USER_AGENT'] ?? '') . "\n";
-echo 'URI: ' . ($_SERVER['REQUEST_URI'] ?? '') . "\n";
-exit;
-
